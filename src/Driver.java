71be4f8fa23e3684ffe36a41751ac88a7900372e
ssavardr <ssavardr@gmail.com>
2015-04-02 13:46:01 -0500

    gui setup

--- src/Driver.java
+++ src/Driver.java
@@ -1,4 +1,12 @@
 
 public class Driver {
 
+	
+	public Driver(){
+		FrontEnd gui = new FrontEnd();
+	}
+	
+	public static void main(String[] args){
+		new Driver();
+	}
 }
--- src/FrontEnd.java
+++ src/FrontEnd.java
@@ -1,4 +1,26 @@
+import java.awt.Dimension;
+
+import javax.swing.JFrame;
+
 
 public class FrontEnd {
-//sean is so stupid 
+
+		JFrame window;
+		
+	public FrontEnd(){
+		window = new JFrame("Messanger");
+		Dimension dim = new Dimension(800,800);
+		window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		window.setVisible(true);
+		window.setPreferredSize(dim);
+		window.setMaximumSize(dim);
+		window.setMinimumSize(dim);
+		window.setLocationRelativeTo(null);
+		
+	}
+	public static void main(String[] args){
+		
+	}
+	
+	
 }
